8214a49df92d6072ec5cfdcea5a696030e1399d7
JAOS-DEV <jaosullivan0@gmail.com>
2025-08-10 22:12:57 +0100

    Fix Tailwind CSS configuration and Firebase setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,19 @@
 export default {
   content: [
     "./index.html",
+    "./index.css",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./App.tsx",
-    "./index.tsx"
+    "./index.tsx",
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#FAF7F0",
+      },
+    },
   },
   plugins: [],
-}
+};
